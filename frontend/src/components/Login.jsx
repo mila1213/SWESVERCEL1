c21bacb5b84ed9bc7a4684a5c41e5f2dbed50b7e
ConcepcionArequipa <concepcion.arequipa@epn.edu.ec>
2026-05-05 23:37:10 -0500

    Finalización de diseño - frontend, donde se integro tailwind como herramienta principal para el diseño

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-
+import logoSwes from '../assets/icono_sistema.png';
 const Login = () => {
   const navigate = useNavigate();
 
@@ -27,16 +27,50 @@ const Login = () => {
   };
 
   return (
-    <div>
-      <h2>Iniciar Sesión</h2>
-      <form onSubmit={handleLogin}>
-        <input name="email" type="email" placeholder="Correo" required />
-       <br />
-        <input name="password" type="password" placeholder="Contraseña" required />
-        <br />
-        <button type="submit">Entrar</button>
+    
+    <div className='min-h-screen bg-[oklch(28.2%_0.091_267.935)] flex items-center justify-center p-4'>
+      <div className='bg-white w-full max-w-md p-8 shadow-2xl rounded-3xl'>
+        <div className="flex items-center justify-center mb-4 select-none">
+          <img src={logoSwes} alt="Logo SWES" className="w-10 h-10 mr-4 object-contain"/>
+          <h1 className="text-3xl tracking-normal font-sans">
+          <span className=" font-bold text-[oklch(28.2%_0.091_267.935)]">SWES</span>
+          <span className="font-bold text-[oklch(82.8%_0.189_84.429)] ml-2">EPN</span>
+          </h1>
+        </div>
+        <h2 className='font-bold text-center text-gray-800 mb-8 text-[30px]'>Iniciar Sesión</h2>
+        <form onSubmit={handleLogin} className="flex flex-col gap-7">
+        <input 
+        name="email" 
+        type="email" 
+        placeholder="Correo electrónico" 
+        required
+        className="w-full p-3 border border-gray-300 rounded-xl outline-none transition-all
+             focus:ring-2 
+             focus:ring-[oklch(90.5%_0.182_98.111)] 
+             focus:border-[oklch(90.5%_0.182_98.111)] text-[19px]"
+        />
+        <input 
+        name="password" 
+        type="password" 
+        placeholder="Contraseña" 
+        required 
+        className="w-full p-3 border border-gray-300 rounded-xl outline-none transition-all
+             focus:ring-2 
+             focus:ring-[oklch(90.5%_0.182_98.111)] 
+             focus:border-[oklch(90.5%_0.182_98.111)] text-[19px]"
+        />
+
+        <button 
+        type="submit"
+        className="w-full bg-[oklch(82.8%_0.189_84.429)] hover:brightness-110 hover:shadow-lg text-gray-900 font-bold py-3 text-[21px] rounded-full transition-all shadow-md mt-2"
+        >Entrar
+        </button>
+        
       </form>
-      <p>¿No tienes cuenta? <Link to="/register">Regístrate</Link></p>
+      <p className="mt-8 text-center text-gray-600 text-sm text-[19px]">¿No tienes cuenta? <Link to="/register" className='text-[oklch(28.2%_0.091_267.935)] hover:underline font-bold transition-all text-[19px]'>Regístrate</Link></p>
+
+      </div>
+
     </div>
   );
 };
--- frontend/src/components/Register.jsx
+++ frontend/src/components/Register.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-
+import logoSwes from '../assets/icono_sistema.png';
 const Register = () => {
   const navigate = useNavigate();
 
@@ -32,24 +32,68 @@ const Register = () => {
   };
 
   return (
-    <div style={{ maxWidth: '400px', margin: 'auto', textAlign: 'center' }}>
-      <h2>Registro</h2>
-      <form onSubmit={handleSubmit}>
-        <input name="nombre" placeholder="Nombre completo" required style={inputStyle} />
-        <br />
-        <input name="email" type="email" placeholder="Correo electrónico" required style={inputStyle} />
-        <br />
-        <input name="password" type="password" placeholder="Contraseña (mín. 6 caracteres)" required style={inputStyle} />
-        <br />
-        <button type="submit" style={buttonStyle}>Registrarse</button>
-      </form>
-      <p>¿Ya tienes cuenta? <Link to="/login">Inicia sesión</Link></p>
+    <div className='min-h-screen bg-[oklch(28.2%_0.091_267.935)] flex items-center justify-center p-4'>
+      
+      <div className='bg-white w-full max-w-md p-8 shadow-2xl rounded-3xl'>
+
+        <div className="flex items-center justify-center mb-4 select-none">
+          <img src={logoSwes} alt="Logo SWES" className="w-10 h-10 mr-4 object-contain"/>
+          <h1 className="text-3xl tracking-normal font-sans">
+            <span className=" font-bold text-[oklch(28.2%_0.091_267.935)]">SWES</span>
+            <span className="font-bold text-[oklch(82.8%_0.189_84.429)] ml-2">EPN</span>
+            </h1>
+        </div>
+        
+        <h2 className='font-bold text-center text-gray-800 mb-8 text-[30px]'>Registrarse</h2>
+
+        <form onSubmit={handleSubmit} className="flex flex-col gap-7">
+          <input 
+          name="nombre" 
+          placeholder="Nombre completo" 
+          required
+          className="w-full p-3 border border-gray-300 rounded-xl outline-none transition-all
+             focus:ring-2 
+             focus:ring-[oklch(90.5%_0.182_98.111)] 
+             focus:border-[oklch(90.5%_0.182_98.111)] text-[19px]"
+          />
+          
+          <input 
+          name="email" 
+          type="email" 
+          placeholder="Correo electrónico" 
+          required
+          className="w-full p-3 border border-gray-300 rounded-xl outline-none transition-all
+             focus:ring-2 
+             focus:ring-[oklch(90.5%_0.182_98.111)] 
+             focus:border-[oklch(90.5%_0.182_98.111)] text-[19px]"
+          />
+          
+          <input 
+          name="password" 
+          type="password" 
+          placeholder="Contraseña (mín. 6 caracteres)" 
+          required
+          className="w-full p-3 border border-gray-300 rounded-xl outline-none transition-all
+             focus:ring-2 
+             focus:ring-[oklch(90.5%_0.182_98.111)] 
+             focus:border-[oklch(90.5%_0.182_98.111)] text-[19px]"
+          />
+          
+          <button 
+          type="submit" 
+          className="w-full bg-[oklch(82.8%_0.189_84.429)] hover:brightness-110 hover:shadow-lg text-gray-900 font-bold py-3 text-[21px] rounded-full transition-all shadow-md mt-2"
+          >Registrarse</button>
+          
+        </form>
+
+        <p className="mt-8 text-center text-gray-600 text-sm text-[19px]">¿Ya tienes cuenta? <Link to="/login" className='text-[oklch(28.2%_0.091_267.935)] hover:underline font-bold transition-all text-[19px]'>Inicia sesión</Link></p>
+      </div>
+      
+      
+      
     </div>
   );
 };
 
-
-const inputStyle = { padding: '10px', margin: '10px 0', width: '100%' };
-const buttonStyle = { padding: '10px 20px', cursor: 'pointer', backgroundColor: '#007bff', color: 'white', border: 'none' };
 
 export default Register;
